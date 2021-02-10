32b519658439a5c12783029b4da4a1a06adf23a5
Rohit <rohitsraj12@gmail.com>
2021-02-10 14:18:27 +0530

    Create fullScreen.js

---
+++ js/fullScreen.js
@@ -0,0 +1,18 @@
+
+$(function() {
+    $.scrollify({
+        section : ".scroll",
+        // interstitialSection : ".body__footer",
+        // easing: "easeOutExpo",
+        scrollSpeed: 100,
+        offset : 0,
+        scrollbars: true,
+        standardScrollElements: "",
+        updateHash: true,
+        touchScroll:true,
+        // before:function() {},
+        // after:function() {},
+        // afterResize:function() {},
+        // afterRender:function() {}
+      });
+  });
