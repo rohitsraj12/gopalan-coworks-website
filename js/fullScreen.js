5422c551305b022d42f4ffc3dac909beeb6d10d8
Rohit <rohitsraj12@gmail.com>
2021-02-20 16:37:40 +0530

    Update fullScreen.js

--- js/fullScreen.js
+++ js/fullScreen.js
@@ -53,3 +53,30 @@ $('.owl-carousel').find('.owl-nav').removeClass('disabled');
 $('.owl-carousel').on('changed.owl.carousel', function(event) {
 	$(this).find('.owl-nav').removeClass('disabled');
 });
+
+// gallery slider
+$('.section__gallery-slider .owl-carousel').owlCarousel({
+    stagePadding: 1,
+    loop:true,
+    margin:20,
+    nav:true,
+    animateOut: 'slideOutUp',
+    animateIn: 'slideInUp',
+    navText:["<div class='d-none nav-btn prev-slide'></div>","<div class='nav-btn next-slide'><span class='link__icon'>&#8594;</span></div>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2.3
+        }
+    }
+})
+
+$('.owl-carousel').find('.owl-nav').removeClass('disabled');
+$('.owl-carousel').on('changed.owl.carousel', function(event) {
+	$(this).find('.owl-nav').removeClass('disabled');
+});
