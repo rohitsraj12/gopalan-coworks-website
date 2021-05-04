15eaf8041d8ef1593e55f20401407d0768f3e033
rohitsraj12 <rohitsraj12@gmail.com>
2021-05-04 22:04:39 +0530

    Create covid_update.php

---
+++ covid_update.php
@@ -0,0 +1,207 @@
+<?php
+
+    $page_title = "covid update";
+
+    include('required/header.inc.php');
+
+?>
+
+
+
+
+
+            <div class="body__container pb-5">
+                <main>
+                    
+                    <section class="section__community-gallery pt-5">
+                        <div class="pt-5">
+                            
+                            <div class="section__community-gallery-header py-5 mt-5">
+                                <h2 class="section__header">
+                                   Covid - 19 Updates
+                                </h2>
+                                
+                                <h4 class="text-center location__header-bottom">
+                                    subheader goes here
+                                </h4>
+                            </div>
+                            
+                            <div class="section__body">
+                                <div class="wrap__container wrap__community-type">
+                                    <div class="community__type text-center">
+                                       <p>
+                                           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit officiis alias quam, beatae itaque veniam dicta nulla sed non possimus facilis, fuga amet iusto velit architecto, modi minus excepturi cupiditate.
+                                       </p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <section class="scroll section__home-one py-5" id="bannerghatta">
+                                <div class="container">
+                                    <div class="row">
+                                    <div class="col-md-6 order-12 order-md-1">
+                                            <article class="h-100 d-flex flex-column justify-content-center py-4 py-md-0">
+                                                <header class="pb-3" data-aos="fade-down" data-aos-duration="1200">
+                                                    <h2 class="section__header-side text-left">
+                                                        BUILDING PREPARDENESS
+                                                    </h2>
+                                                    <h4 class="sub__head_location">Steps taken prior to resuming operations</h4>
+                                                </header>
+                                                <div class="section__body"  data-aos="fade-up" data-aos-duration="1200">
+                                                    <p>
+                                                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi inventore perferendis, repudiandae rerum, perspiciatis facilis, sed eveniet reiciendis quia nesci
+                                                    </p>
+                                                    <ul>
+                                                        <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore, esse?</li>
+                                                        <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</li>
+                                                        <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem, obcaecati nisi. Dolorum?</li>
+                                                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing.</li>
+                                                    </ul>
+                                                </div>
+                                                <!-- <footer class="pt-3 text-left" data-aos="fade-up" data-aos-duration="1400">
+                                                    <a class="link__primary" href="location/bannerghatta_road">
+                                                        read more <span class="link__icon">&#8594;</span> 
+                                                    </a>
+                                                </footer> -->
+                                            </article>
+                                    </div>
+
+                                    <div class="col-md-6 order-md-12 order-1"  data-aos="fade-left" data-aos-duration="1000">
+                                            <figure>
+                                                <img class="w-100" src="img/about/bannerghatta-road.jpg" alt="">
+                                            </figure>
+                                    </div>
+                                    </div>
+                                    
+                                    
+                                    <div class="container__scroll-down">
+                                        <div class="wrap__scroll">
+                                            <a href="#banashankari"><span>&#8595;</span></a>
+                                        </div>
+                                        <!-- end wrap scroll -->
+                                    </div>
+                                    <!-- end scroll down -->
+                                </div>
+                            </section>
+                            <!-- end section highlight -->
+                            
+                            <section class="scroll section__home-one py-5" id="banashankari">
+                                <div class="container">
+                                    <div class="row">
+
+                                    <div class="col-md-6 order-md-12"  data-aos="fade-right" data-aos-duration="1000">
+                                            <figure>
+                                                <img class="w-100" src="img/about/banashnakari.jpg" alt="">
+                                            </figure>
+                                    </div>
+                                    <div class="col-md-6 order-12">
+                                            <article class="h-100 d-flex flex-column justify-content-center py-4 py-md-0">
+                                                <header class="pb-3" data-aos="fade-down" data-aos-duration="1200">
+                                                    <h2 class="section__header-side text-left">
+                                                        THERMAL SCREENING
+                                                    </h2>
+                                                    
+                                                    <h4 class="sub__head_location">Thermal Scanning should be done for everyone entering the premises</h4>
+                                                </header>
+                                                <div class="section__body"  data-aos="fade-up" data-aos-duration="1200">
+                                                    <p>
+                                                       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni, doloremque repellendus. Corrupti, dicta? Soluta doloremque consequatur placeat impedit sit iure?
+                                                    </p>
+                                                    <p>
+                                                       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni, doloremque repellendus. Corrupti, dicta? Soluta doloremque consequatur placeat impedit sit iure?
+                                                    </p>
+                                                </div>
+                                                <!-- <footer class="pt-3 text-left" data-aos="fade-up" data-aos-duration="1400">
+                                                    <a class="link__primary" href="location/banashankari">
+                                                        read more <span class="link__icon">&#8594;</span> 
+                                                    </a>
+                                                </footer> -->
+                                            </article>
+                                    </div>
+                                    </div>
+                                    
+                                    <div class="container__scroll-down">
+                                        <div class="wrap__scroll">
+                                            <a href="#old-madras-road"><span>&#8595;</span></a>
+                                        </div>
+                                        <!-- end wrap scroll -->
+                                    </div>
+                                    <!-- end scroll down -->
+                                </div>
+                            </section>
+                            <!-- end section highlight -->
+
+                            <section class="scroll section__home-one py-5" id="old-madras-road">
+                                <div class="container">
+                                    <div class="row">
+                                    <div class="col-md-6 order-12 order-md-1">
+                                            <article class="h-100 d-flex flex-column justify-content-center py-4 py-md-0">
+                                                <header class="pb-3" data-aos="fade-down" data-aos-duration="1200">
+                                                    <h2 class="section__header-side text-left">
+                                                        SOP FOR COVID -19
+                                                    </h2>
+                                                    <h4 class="sub__head_location">Workplace Safety and Measures that need to be followed:</h4>
+                                                </header>
+                                                <div class="section__body"  data-aos="fade-up" data-aos-duration="1200">
+                                                    <!-- <p>
+                                                        Efficient and comfortable, this coworking space is the first in the series of the Gopalan Coworks. With a total of 300+ seats including hot desks, dedicated desks, private cabins or meeting rooms. This plug and play office space in Bangalore is perfect for a work-life balance. A team dinner or a movie night after a hectic day, this space offers everything for your team under one roof.
+                                                    </p> -->
+                                                    <ul>
+                                                        <li>
+                                                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium obcaecati dicta voluptates quisquam!
+                                                        </li>
+                                                        <li>
+                                                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium obcaecati dicta voluptates quisquam!
+                                                        </li>
+                                                        <li>
+                                                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium obcaecati dicta voluptates quisquam!
+                                                        </li>
+                                                        <li>
+                                                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium obcaecati dicta voluptates quisquam!
+                                                        </li>
+                                                        <li>
+                                                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium obcaecati dicta voluptates quisquam!
+                                                        </li>
+                                                        <li>
+                                                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium obcaecati dicta voluptates quisquam!
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                                <!-- <footer class="pt-3 text-left" data-aos="fade-up" data-aos-duration="1400">
+                                                    <a class="link__primary" href="location/old_madras_road">
+                                                        read more <span class="link__icon">&#8594;</span> 
+                                                    </a>
+                                                </footer> -->
+                                            </article>
+                                    </div>
+
+                                    <div class="col-md-6 order-md-12 order-1"  data-aos="fade-left" data-aos-duration="1000">
+                                            <figure>
+                                                <img class="w-100" src="img/about/old-madras-road.jpg" alt="">
+                                            </figure>
+                                    </div>
+                                    </div>
+                                    
+                                    <!-- <div class="container__scroll-down">
+                                        <div class="wrap__scroll">
+                                            <a href="#commercial-street"><span>&#8595;</span></a>
+                                        </div>
+                                    </div> -->
+                                    <!-- end scroll down -->
+                                </div>
+                            </section>
+                            <!-- end section highlight -->
+                        </div>
+                    </section>
+                </main>
+            </div>
+            <!-- end body container -->
+
+
+
+
+<?php 
+
+    include('required/footer.inc.php');
+
+?>
