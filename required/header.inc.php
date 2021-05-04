1f22ee35b5e36a7b2f26962c9de9a4ea4d4b45ea
rohitsraj12 <rohitsraj12@gmail.com>
2021-05-04 22:04:50 +0530

    Create header.inc.php

---
+++ required/header.inc.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?php echo $page_title; ?> | Gopalan Coworks</title>
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="author" content="">
+        <meta name="distribution" content="global" />
+        <meta http-equiv="content-language" content="en-gb">
+        <meta name="city" content="bangalore" />
+        <meta name="geo.region" content="IN" />
+        <meta name="state" content="Karnataka" />
+        <meta name="twitter:card" content="summary">
+        <meta name="twitter:creator" content="@"/>
+        <meta name="twitter:description" content="" />
+        <meta name="twitter:keywords" content="">
+        
+        <!-- open graph code begins-->
+        <meta property="og:type" content="business.business">
+        <meta property="og:title" content=" ">
+        <meta property="og:url" content="">
+        <meta property="business:contact_data:street_address" content="Richmond Road">
+        <meta property="business:contact_data:locality" content="Bangalore">
+        <meta property="business:contact_data:region" content="Karnataka">
+        <meta property="business:contact_data:postal_code" content="560025">
+        <meta property="business:contact_data:country_name" content="India">
+        <link rel="shortcut icon" type="image/png" href="img/brand/favicon.png" />
+        
+        <!-- frame-work/plug-in stylesheet -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.2.3/animate.min.css">
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+        <!-- custom stylesheet -->
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/home-2.css">
+        <link rel="stylesheet" href="css/gallery.css">
+        <link rel="stylesheet" href="css/responsive.css">
+    </head>
+
+    <body>
+        <div class="body__wrap" id="home">
+
+            <div class="body__header" id="body-header">
+                <div class="top__header bg-danger text-center py-1">
+                    <a href="" class="text-light">
+                        COVID-19 update
+                    </a>
+                </div>
+                <!-- end top header -->
+                <div class="hamberger" id="hamberger">
+                    <svg width="40" height="40" viewBox="0 0 100 100">
+                        <path class="line line1" d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058" />
+                        <path class="line line2" d="M 20,50 H 80" />
+                        <path class="line line3" d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942" />
+                      </svg>
+                      <i class="fa fa-bars"></i>
+                </div>
+
+                <header class="wrap__container">
+                    <div class="header__brand">
+                        <a href="index">
+                            <img src="img/brand/Logo-1.png" class="d-banner-image" alt="Gopalan Coworks Logo">
+                            <img src="img/brand/Logo-1-sm.png" class="m-banner-image" alt="Gopalan Coworks Logo">
+                        </a>
+                    </div>
+                    <!-- end header brand -->
+                    <nav class="header__nav" id="header-nav">
+                        <ul>
+                            <li class="nav__list"><a href="index" class="nav__link">Home</a></li>
+                            <li class="nav__list"><a class="nav__link">Locations <i class="fa fa-angle-down pl-2" aria-hidden="true"></i></a>
+                                <div class="sub__nav">
+                                    <ul>
+                                        <li class="sub-nav__list"><a href="location/banashankari" class="sub-nav__link">Banashankari</a></li>
+                                        <li class="sub-nav__list"><a href="location/bannerghatta_road" class="sub-nav__link">Bannerghatta Road</a></li>
+                                        <li class="sub-nav__list"><a href="location/brookefield" class="sub-nav__link">Brookefield</a></li>
+                                        <li class="sub-nav__list"><a href="location/commercial_street" class="sub-nav__link">Commercial Street</a></li>
+                                        <li class="sub-nav__list"><a href="location/old_madras_road" class="sub-nav__link">Old Madras Road</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li class="nav__list"><a href="about_us" class="nav__link active__nav ">About Us</a></li>
+                            <li class="nav__list"><a href="community" class="nav__link">Community</a></li>
+                            <li class="nav__list"><a href="contact_us" class="nav__link">Contact Us</a></li>
+                            <li class="nav__list"><a href="" class="nav__link nav-btn" data-toggle="modal" data-target="#quote">Get A Quote</a></li>
+                        </ul>
+                    </nav>
+                    <!-- end header nav -->
+                </header>
+            </div>
+            <!-- end body header -->
