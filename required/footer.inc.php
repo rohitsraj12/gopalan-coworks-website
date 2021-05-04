85893457c20041b23a7cf2000d4af0436ce83c41
rohitsraj12 <rohitsraj12@gmail.com>
2021-05-04 22:04:47 +0530

    Create footer.inc.php

---
+++ required/footer.inc.php
@@ -0,0 +1,269 @@
+
+            <div class="body__footer">
+                <footer class="wrap__container pb-4">
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <article>
+                                <header>
+                                    About us
+                                </header>
+                                <p>
+                                    Gopalan Enterprises, a pioneer in Bangalore′s real estate, comprises a team of dynamic and vibrant designers and construction professionals.
+                                </p>
+                                <ul class="social__media">
+                                    <li>
+                                        <a href="https://www.facebook.com/gopalancoworks/" target="_blank"><i class="fa fa-facebook-f"></i></a>
+                                    </li>
+                                    <li>
+                                        <a href="https://twitter.com/gopalancoworks" target="_blank"><i class="fa fa-twitter"></i></a>
+                                    </li>
+                                    <li>
+                                        <a href="https://www.instagram.com/gopalancoworks/" target="_blank"><i class="fa fa-instagram"></i></a>
+                                    </li>
+                                    <li>
+                                        <a href="https://in.linkedin.com/company/gopalan-coworks" target="_blank"><i class="fa fa-linkedin"></i></a>
+                                    </li>
+                                </ul>
+                            </article>
+                        </div>
+                        <div class="col-sm-3">
+                            <article>
+                                <header>
+                                    Get in touch
+                                </header>
+                                <ul>
+                                    <li>
+                                        Phone : <a href="tel:080 4931 4961">080 4931 4961</a>
+                                    </li>
+                                    <li>
+                                        Email : <a href="mailto:sales@gopalancoworks.com" target="_blank">sales@gopalancoworks.com</a>
+                                    </li>
+                                </ul>
+                            </article>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <article>
+                                        <header>
+                                            Quick Links
+                                        </header>
+                                        <ul>
+                                            <li><a href="index">Home</a></li>
+                                            <li><a href="about_us">About Us</a></li>
+                                            <li><a href="community">Community</a></li>
+                                            <li><a href="https://www.gopalancoworks.com/blog/">Blogs</a></li>
+                                            <li><a href="faq">FAQ</a></li>
+                                            <li><a href="contact_us">Contact Us</a></li>
+                                        </ul>
+                                    </article>
+                                </div>
+                                <div class="col-sm-6">
+                                    <article>
+                                        <header>
+                                            Our Locations
+                                        </header>
+                                        <ul>
+                                            <li><a href="location/banashankari">Banashankari</a></li>
+                                            <li><a href="location/bannerghatta_road">Bannerghatta Road</a></li>
+                                            <li><a href="location/brookefield">Brookefield</a></li>
+                                            <li><a href="location/commercial_street">Commercial Street</a></li>
+                                            <li><a href="location/old_madras_road">Old Madras Road</a></li>
+                                        </ul>
+                                    </article>
+                                </div>
+                            </div>
+                            
+                        </div>
+                        <div class="col-sm-2">
+                            <article>
+                                <header>
+                                    Head Office
+                                </header>
+                                <p>
+                                    <address>
+                                        #5 Rich Home, 1st Floor, Gopalan Coworks(department), Gopalan Enterprises, Richmond Road, Bengaluru, Karnataka 560025
+                                    </address>
+                                </p>
+                            </article>
+                        </div>
+                    </div>
+                </footer>
+                <div class="copy__right pt-2 border-top border-dark">
+                    <p class="text-center">
+                        Copyright &copy;<script>document.write(new Date().getFullYear());</script> | <a href="https://www.gopalancoworks.com/">gopalancoworks.com</a> | <a href="Privacy_policy">Privacy Policy</a>
+                    </p>
+                </div>
+                <div class="footer__fixed">
+                    <div class="fixed__whatsApp arrow-container animated fadeInDown" id="">
+                        <div class="arrow-1 animated hinge infinite zoomIn"></div>
+                        <div class="button">
+                            <a target="_blank" href="https://api.whatsapp.com/send?phone=+919353915170&text=Hi!%20I%20am%20Interested.%20Share%20details."> <i class="fa fa-whatsapp"></i></a>
+                        </div>
+                    </div>
+                
+                    <div class="fixed__contact">
+                        <ul>
+                            <li>
+                                <a href="tel:080 4931 4961">Call Us</a>
+                            </li>
+                            <li>
+                                <a href="" class=""  data-toggle="modal" data-target="#quote">Get Quote</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="fixed__bottom hide">
+                    <ul> 
+                        <li><a href="tel:080 4931 4961">Call Us</a></li>
+                    
+                        <li><a class="whats__app" target="_blank" href="https://api.whatsapp.com/send?phone=+919353915170&text=Hi!%20I%20am%20Interested.%20Share%20details."> <i class="fa fa-whatsapp"></i></a></li>
+                       
+                        <li><a href="" data-toggle="modal" data-target="#enquire">Enquire Now</a></li>
+                    </ul>
+                </div>
+                <!-- end fixed bottom -->
+            </div>
+            <!-- end body footer -->
+        </div>
+        <!-- end body wrap -->
+
+        <!-- enquire form -->
+        <div class="modal fade" id="enquire" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered ">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="staticBackdropLabel">Equiry Form</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="enquire.php" method="POST" class="banner__form w-100 py-0 px-2">
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <input type="text" class="form-control" name="name" placeholder="Your name" id="name" aria-describedby="emailHelp">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="tel">Mobile no</label>
+                                <input type="tel" class="form-control" name="phone"  id="tel" placeholder="Your mobile" aria-describedby="emailHelp">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="email">Email address</label>
+                                <input type="email" class="form-control" name="email"  id="email"  placeholder="Your email" aria-describedby="emailHelp">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="location">Your Location</label>
+                                <select name="location" class="form-control" id="location">
+                                    <option>Select Your Locaton</option>
+                                    <option value="Banashankari" >Banashankari</option>
+                                    <option value="Bannerghatta road">Bannerghatta Road</option>
+                                    <option value="Brooke Field">Brookefield</option>
+                                    <option value="Commercial Street">Commercial Street</option>
+                                    <option value="Old Madras Road">Old Madras Road</option>
+                                </select>
+                            </div>
+                            
+                            <div class="form-group">
+                                <label for="seats">Number Of Seats</label>
+                                <select name="seats" class="form-control" id="seats">
+                                    <option>Select Number of Seats</option>
+                                    <option value="1-5" >01 - 05 Seats</option>
+                                    <option value="6-15">06 - 15 Seats</option>
+                                    <option value="16-25">16 - 25 Seats</option>
+                                    <option value="26-50">26 - 50 Seats</option>
+                                    <option value="51+">51 + Seats</option>
+                                </select>
+                            </div>
+                            <!-- enter form type -->
+                            <input type="hidden" name="enquire" value="Enquire">
+
+                            <div class="text-center">
+                                <input type="submit" name="submit" class="btn__primary">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- end model emquire form -->
+
+        <!-- get quote form -->
+        <div class="modal fade" id="quote" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered ">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="staticBackdropLabel">Get Quote</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="enquire.php" method="POST" class="banner__form w-100 py-0 px-2">
+                            <!-- <header class="text-center h4 pb-3">
+                            Enquire Form
+                            </header> -->
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <input type="text" class="form-control" name="name" placeholder="Your name" id="name" aria-describedby="emailHelp">
+                            </div>
+                            <div class="form-group">
+                                <label for="tel">Mobile no</label>
+                                <input type="tel" class="form-control" name="phone"  id="tel" placeholder="Your mobile" aria-describedby="emailHelp">
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Email address</label>
+                                <input type="email" class="form-control" name="email"  id="email"  placeholder="Your email" aria-describedby="emailHelp">
+                            </div>
+                            <div class="form-group">
+                            <label for="email">Your Location</label>
+                                <select name="location" class="form-control" id="exampleFormControlSelect1">
+                                <option>Select Your Locaton</option>
+                                <option value="Banashankari" >Banashankari</option>
+                                <option value="Bannerghatta road">Bannerghatta Road</option>
+                                <option value="Brooke Field">Brookefield</option>
+                                <option value="Commercial Street">Commercial Street</option>
+                                <option value="Old Madras Road">Old Madras Road</option>
+                                </select>
+                            </div>
+                            
+                            <div class="form-group">
+                                <label for="seats">Number Of Seats</label>
+                                <select name="seats" class="form-control" id="seats">
+                                    <option>Select Number of Seats</option>
+                                    <option value="1-5" >01 - 05 Seats</option>
+                                    <option value="6-15">06 - 15 Seats</option>
+                                    <option value="16-25">16 - 25 Seats</option>
+                                    <option value="26-50">26 - 50 Seats</option>
+                                    <option value="51+">51 + Seats</option>
+                                </select>
+                            </div>
+                            
+                            <!-- enter form type -->
+                            <input type="hidden" name="enquire" value="Get Quote">
+                            <div class="text-center">
+                                <input type="submit" name="submit" class="btn__primary">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- end model quote form -->
+
+        <!-- script -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>    
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/scrollify/1.0.4/jquery.scrollify.min.js"></script>
+
+        <!-- custom js script -->
+        <script src="js/fullScreen.js"></script>  
+        <script src="js/script.js"></script>    
+    </body>
+</html>
+
